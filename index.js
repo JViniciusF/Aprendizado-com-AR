e5e873f39ac37a82e34d46c52a49bbc2d2fa05b1
J Vinicius Ferreira Santana <48533842+JViniciusF@users.noreply.github.com>
2020-03-10 11:16:12 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+window.onload =function(){
+    var marcadorTeste = document.getElementById("ba");
+    marcadorTeste.addEventListener("markerFoud",function(){
+        alert("Reconheceu")
+    });
+}
