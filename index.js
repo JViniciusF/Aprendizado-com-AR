299772fbebe9f88d573e0255080860a0529f2f54
JViniciusF <jose.santana20@fatec.sp.gov.br>
2020-03-11 20:28:57 -0300

    Atualizando Marcadores

---
+++ index.js
@@ -0,0 +1,9 @@
+window.onload =function(){
+    var marcadorTeste = document.getElementById("a");
+    console.log("Fudeu geral");
+    marcadorTeste.addEventListener("makerFound",function(){
+        console.log("Achou");
+    });
+    
+}
+
